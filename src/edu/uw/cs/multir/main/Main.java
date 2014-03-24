736e0ad78f15b4ec8642397b447b71e7946d2fc0
ajaynagesh <ajaynagesh@gmail.com>
2014-03-24 16:59:11 +0530

    test

--- src/edu/uw/cs/multir/main/Main.java
+++ src/edu/uw/cs/multir/main/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+		System.out.println("test comment");
 		if (args.length == 0)
 			printUsage();
 		
